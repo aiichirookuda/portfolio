add913a89abb468da09e4a93c5d05e6f8aaffa56
aiichiro <aiichirookuda@gmail.com>
2021-11-02 03:08:59 +0900

    Update 2021-11-01T18:08:57.880Z

--- assets/dist/scripts/thumbnail.js
+++ assets/dist/scripts/thumbnail.js
@@ -1,12 +1,14 @@
 'use strict';
 
-document.querySelectorAll('.js-thumbnail').forEach(function (el) {
-  el.addEventListener('mouseover', function () {
-    el.children[0].classList.add('is-active');
-    el.children[1].classList.add('is-active');
+if (!navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+  document.querySelectorAll('.js-thumbnail').forEach(function (el) {
+    el.addEventListener('mouseover', function () {
+      el.children[0].classList.add('is-active');
+      el.children[1].classList.add('is-active');
+    });
+    el.addEventListener('mouseout', function () {
+      el.children[0].classList.remove('is-active');
+      el.children[1].classList.remove('is-active');
+    });
   });
-  el.addEventListener('mouseout', function () {
-    el.children[0].classList.remove('is-active');
-    el.children[1].classList.remove('is-active');
-  });
-});
+}
