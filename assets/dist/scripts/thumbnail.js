0215207285bafbaaa499ec11ba9d2380e59fdb70
aiichiro <aiichirookuda@gmail.com>
2021-11-02 05:39:48 +0900

    Update 2021-11-01T20:39:46.885Z

--- assets/dist/scripts/thumbnail.js
+++ assets/dist/scripts/thumbnail.js
@@ -1,14 +1,12 @@
 'use strict';
 
-if (!navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-  document.querySelectorAll('.js-thumbnail').forEach(function (el) {
-    el.addEventListener('mouseover', function () {
-      el.children[0].classList.add('is-active');
-      el.children[1].classList.add('is-active');
-    });
-    el.addEventListener('mouseout', function () {
-      el.children[0].classList.remove('is-active');
-      el.children[1].classList.remove('is-active');
-    });
+document.querySelectorAll('.js-thumbnail').forEach(function (el) {
+  el.addEventListener('mouseover', function () {
+    el.children[0].classList.add('is-active');
+    el.children[1].classList.add('is-active');
   });
-}
+  el.addEventListener('mouseout', function () {
+    el.children[0].classList.remove('is-active');
+    el.children[1].classList.remove('is-active');
+  });
+});
